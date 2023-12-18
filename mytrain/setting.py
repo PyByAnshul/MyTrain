761b97de090210d510334bb5503674a6acf44364
anshul kumar <96117083+anshulkashyap1234@users.noreply.github.com>
2023-12-18 21:26:30 +0530

    'pymongo'

--- mytrain/setting.py
+++ mytrain/setting.py
@@ -1,12 +1,19 @@
 from app import app
 from flask_mongoengine import MongoEngine
 from config import Config
-from flask_pymongo import PyMongo
-#congiration integraion
-app.config.from_object(Config)
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+uri = "mongodb+srv://a9756549615:oNOccgAMhlA79wHW@cluster0.mak8f9p.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+#congiration integraion
+
+app.config.from_object(Config)
 #mongo database
 dbs = MongoEngine(app)
-client=PyMongo(app).db
-mydb=client
+
+mydb=client.mytrain
 #import apps here
